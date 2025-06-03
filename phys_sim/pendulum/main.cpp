4d6c7ddbdb341c59cf2b1f035345d097a3f24dbe
catalyst <39456203+catalyst2001@users.noreply.github.com>
2025-06-04 01:38:33 +0300

    updates
    object picking (added stencil buffer with functions for get position in world coords)
    fixed drawing transforms

--- phys_sim/pendulum/main.cpp
+++ phys_sim/pendulum/main.cpp
@@ -13,29 +13,7 @@
 #include "wavefront_obj.h"
 #include "node.h"
 
-SDL_Window* pwindow;
-SDL_GLContext ctx;
-obj_importer obj;
-float current_time;
-
 #define arrsize(x) (sizeof(x) / sizeof(x[0]))
-
-glm::vec3 barycenter(bbox_t &dst, const glm::vec3 *pverts, size_t count)
-{
-  glm::vec3 total(0.f, 0.f, 0.f);
-  dst.init();
-  for (size_t i = 0; i < count; i++) {
-    total += pverts[i];
-    dst.update(pverts[i]);
-  }
-  return total / float(count);
-}
-
-void transform_verts(glm::vec3* pverts, size_t count, const glm::mat4x4 &mat)
-{
-  for (size_t i = 0; i < count; i++)
-    pverts[i] = mat * glm::vec4(pverts[i], 1.f);
-}
 
 model_node nodes_hierarchy[] = {
   model_node(-1, "node0", { 0.f, 0.f, 0.f }, { 0.f, 0.f, 0.f }),
@@ -49,6 +27,44 @@ model_node nodes_hierarchy[] = {
   model_node(3,  "node8", { 0.f, 0.f, 0.f }, { 0.f, 0.f, 0.f })
 };
 
+SDL_Window*   pwindow;
+SDL_GLContext ctx;
+obj_importer  obj;
+float         current_time;
+glm::ivec2    g_mouse;
+glm::ivec4    g_viewport;
+glm::vec3     centers[arrsize(nodes_hierarchy)];
+GLUquadric*   g_pquadratic = nullptr;
+
+/**
+* barycenter
+* 
+*/
+glm::vec3 barycenter(bbox_t &dst, const glm::vec3 *pverts, size_t count)
+{
+  glm::vec3 total(0.f, 0.f, 0.f);
+  dst.init();
+  for (size_t i = 0; i < count; i++) {
+    total += pverts[i];
+    dst.update(pverts[i]);
+  }
+  return total / float(count);
+}
+
+/**
+* transform_verts
+*
+*/
+void transform_verts(glm::vec3* pverts, size_t count, const glm::mat4x4 &mat)
+{
+  for (size_t i = 0; i < count; i++)
+    pverts[i] = mat * glm::vec4(pverts[i], 1.f);
+}
+
+/**
+* find_node
+*
+*/
 model_node* find_node(const char *pname)
 {
   for (auto &pnode : nodes_hierarchy) {
@@ -57,6 +73,21 @@ model_node* find_node(const char *pname)
     }
   }
   return nullptr;
+}
+
+/**
+* get_node_index
+*
+*/
+int get_node_index(const char* pname)
+{
+  for (int i = 0; i < arrsize(nodes_hierarchy); i++) {
+    model_node* pnode = &nodes_hierarchy[i];
+    if (!strcmp(pnode->get_name(), pname)) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
@@ -93,53 +124,85 @@ void print_childs(const model_node* pnodes, int count)
   printf("\n");
 }
 
-glm::vec3 centers[arrsize(nodes_hierarchy)];
-
-GLUquadric* psphere = nullptr;
-
-void change_coord_system(model_node* pnodes, int count, const obj_importer *pimp)
+/**
+* change_coord_system
+*
+*/
+void print_nodes_barycenter(const obj_importer* pimp)
 {
-  model_node* pnode, *pparent;
+  bbox_t AABB;
+  glm::vec3 null_center;
   obj_importer::mesh* pmesh;
-  /* compute all nodes center */
-  for (int i = 0; i < count; i++) {
-    pnode = &pnodes[i];
+  model_node* pnode;
+  assert(pimp->get_num_meshes() == arrsize(nodes_hierarchy) && "nodes hierarchy incompatible for this model!");
+  printf("=== print_nodes_barycenter ===\n");
+  for (size_t i = 0; i < pimp->get_num_meshes(); i++) {
     pmesh = pimp->get_mesh(i);
-    pparent = &pnodes[pnode->get_parent_id()];
+    pnode = &nodes_hierarchy[i];
+    glm::vec3 null_center = barycenter(AABB, pmesh->get_verts(), pmesh->get_num_verts());
+    printf("node %zd (%s)  local_center_after_shift(%.3f, %.3f, %.3f)  AABB{ min( %f %f %f ); max( %f %f %f ) }\n", 
+      i, pnode->get_name(),
+      null_center.x, null_center.y, null_center.z,
+      AABB.vec_min.x, AABB.vec_min.y, AABB.vec_min.z, 
+      AABB.vec_max.x, AABB.vec_max.y, AABB.vec_max.z);
+  }
+  printf("============================\n\n");
+}
+
+/**
+* change_coord_system
+*
+*/
+void change_coord_system(model_node* pnodes, int count, const obj_importer* pimp)
+{
+  for (int i = 0; i < count; i++) {
+    model_node* pnode = &pnodes[i];
+    obj_importer::mesh* pmesh = pimp->get_mesh(i);
     centers[i] = barycenter(pnode->get_bbox(), pmesh->get_verts(), pmesh->get_num_verts());
   }
 
-#if 1
   for (int i = 0; i < count; i++) {
-    glm::vec3 delta;
-    pnode = &pnodes[i];
-    pmesh = pimp->get_mesh(i);
-    pparent = &pnodes[pnode->get_parent_id()];
-    delta = (pnode->get_parent_id() != -1) ? centers[i] - centers[pnode->get_parent_id()] : glm::vec3(0.f, 0.f, 0.f);
-    pnode->set_position(delta);
+    model_node* pnode = &pnodes[i];
+    obj_importer::mesh* pmesh = pimp->get_mesh(i);
+    int parent_id = pnode->get_parent_id();
+    model_node* pparent = (parent_id >= 0 ? &pnodes[parent_id] : nullptr);
+    if (strcmp(pnode->get_name(), "node3") == 0 && pparent) {
+      glm::vec3 parent_min = pparent->get_bbox().vec_min;
+      glm::vec3 parent_max = pparent->get_bbox().vec_max;
+      glm::vec3 pend_min = pnode->get_bbox().vec_min;
+      glm::vec3 pend_max = pnode->get_bbox().vec_max;
+      // compute intersection
+      glm::vec3 inter_min(
+        glm::max(parent_min.x, pend_min.x),
+        glm::max(parent_min.y, pend_min.y),
+        glm::max(parent_min.z, pend_min.z)
+      );
 
-    /* handle pendulum node */
-    //if (!strcmp(pnode->get_name(), "node3")) {
-    //  /* find top of the model */
-    //  assert(pnode->get_parent_id() != -1 && "parent node is -1! this is root");
-    //  model_node* pparent = &pnodes[pnode->get_parent_id()];
-    //  const glm::vec3 &vmin = pnode->get_bbox().vec_min;
-    //  const glm::vec3 &vmax = pnode->get_bbox().vec_max;
-    //  const glm::vec3& pos = pnode->get_pos();
-    //  printf("found pendulum node!  pos( %f %f %f )  bbox{ min( %f %f %f ), max( %f %f %f ) }\n", 
-    //    pos.x, pos.y, pos.z,
-    //    vmin.x, vmin.y, vmin.z,
-    //    vmax.x, vmax.y, vmax.z);
-    //  glm::vec3 attach_pos = glm::min(pparent->get_bbox().vec_max, pnode->get_bbox().vec_max);
-    //}
+      glm::vec3 inter_max(
+        glm::min(parent_max.x, pend_max.x),
+        glm::min(parent_max.y, pend_max.y),
+        glm::min(parent_max.z, pend_max.z)
+      );
+
+      // no empty intersection?
+      if (inter_min.x <= inter_max.x && inter_min.y <= inter_max.y && inter_min.z <= inter_max.z) {
+        centers[i] = glm::vec3(
+          (inter_min.x + inter_max.x) * 0.5f, //X center of intersection
+          inter_max.y, //intersection bound
+          (inter_min.z + inter_max.z) * 0.5f //Z center of intersection
+        );
+      }
+      else {
+        /* print error */
+        printf("Warning: %s bbox not intersects with %s!!!\n", pnode->get_name(), pparent->get_name());
+        __debugbreak();
+      }
+    }
+
+    // compute position of parent. (0,0,0) coord for root node
+    pnode->set_position(pparent ? (centers[i] - centers[parent_id]) : glm::vec3(0.f, 0.f, 0.f));
     transform_verts(pmesh->get_verts(), pmesh->get_num_verts(), glm::translate(glm::mat4x4(1.f), -centers[i]));
-
-    bbox_t govno;
-    glm::vec3 null_center = barycenter(govno, pmesh->get_verts(), pmesh->get_num_verts());
-    printf("node %d   null_center= ( %f %f %f )\n", i, null_center.x, null_center.y, null_center.z);
-    //assert(null_center == glm::vec3(0.f, 0.f, 0.f) && "verts translation incorrect");
   }
-#endif
 }
 
 class pendulum_vis
@@ -167,8 +230,17 @@ void update_viewport(int width, int height)
   glLoadIdentity();
   gluPerspective(45.f, width / (double)height, 1.0, 1000.0);
   glMatrixMode(GL_MODELVIEW);
+
+  g_viewport.x = 0;
+  g_viewport.y = 0;
+  g_viewport.z = width;
+  g_viewport.w = height;
 }
 
+/**
+* get_coord_from_depth
+* 
+*/
 bool get_coord_from_depth(glm::vec3 &dst, glm::ivec4 viewport, glm::ivec2 coord)
 {
   float depth;
@@ -192,13 +264,20 @@ bool get_coord_from_depth(glm::vec3 &dst, glm::ivec4 viewport, glm::ivec2 coord)
   return true;
 }
 
-int get_id_from_coord(glm::ivec4 viewport, glm::ivec2 coord)
+/**
+* get_id_from_coord
+* 
+*/
+int get_id_from_coord(glm::vec3 &world_pos, glm::ivec4 viewport, glm::ivec2 coord)
 {
-  int stencil_value;
-  glm::vec3 world_pos;
+  GLubyte stencil_value;
+#if 1
+  /* inverse Y coord */
+  coord.y = (viewport.w - 1) - coord.y;
+#endif
   if (get_coord_from_depth(world_pos, viewport, coord)) {
     glReadPixels(coord.x, coord.y, 1, 1, GL_STENCIL_INDEX, GL_UNSIGNED_BYTE, &stencil_value);
-    return stencil_value;
+    return static_cast<int>(stencil_value);
   }
   return -1;
 }
@@ -228,6 +307,12 @@ bool handle_events()
       break;
     }
 
+    case SDL_MOUSEMOTION:
+      g_mouse.x = event.motion.x;
+      g_mouse.y = event.motion.y;
+      printf("mouse motion: %d %d\n", g_mouse.x, g_mouse.y);
+      break;
+
     default:
       break;
     }
@@ -243,14 +328,15 @@ void draw_recursive(int myid)
 
   glPushMatrix();
   model_node* pnode = &nodes_hierarchy[myid];
+  const glm::vec3& pos = pnode->get_pos();
+  glTranslatef(pos.x, pos.y, pos.z);
   const glm::vec3 angles = pnode->get_rotation();
   glRotatef(angles.x, 1.f, 0.f, 0.f);
   glRotatef(angles.y, 0.f, 1.f, 0.f);
   glRotatef(angles.z, 0.f, 0.f, 1.f);
-  const glm::vec3& pos = pnode->get_pos();
-  glTranslatef(pos.x, pos.y, pos.z);
 
   obj_importer::mesh* pmesh = obj.get_mesh(myid);
+  glStencilFunc(GL_ALWAYS, myid, 0xFF);
   glVertexPointer(3, GL_FLOAT, 0, pmesh->get_verts());
   glNormalPointer(GL_FLOAT, 0, pmesh->get_normals());
   glDrawArrays(GL_TRIANGLES, 0, pmesh->get_num_verts());
@@ -270,7 +356,100 @@ void draw_recursive(int myid)
 
 void draw_model()
 {
+  glStencilMask(0xFF); //enable stencil writing
   draw_recursive(0);
+  glStencilMask(0x00); //disable writing
+}
+
+void draw_debug_sphere(glm::vec3 pos, glm::vec3 color)
+{
+  GLint old_depth_mode;
+  GLboolean lighting_enabled = glIsEnabled(GL_LIGHTING);
+  if (lighting_enabled)
+    glDisable(GL_LIGHTING);
+
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  glGetIntegerv(GL_DEPTH_FUNC, &old_depth_mode);
+  glDepthFunc(GL_ALWAYS);
+  glPushAttrib(GL_CURRENT_BIT);
+  glColor3fv(glm::value_ptr(color));
+  glTranslatef(pos.x, pos.y, pos.z);
+  gluSphere(g_pquadratic, 0.5, 10, 10);
+  glTranslatef(-pos.x, -pos.y, -pos.z);
+  glPopAttrib();
+  glDepthFunc(old_depth_mode);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+  if (lighting_enabled) {
+    glEnable(GL_LIGHTING);
+  }
+}
+
+void draw_centers()
+{
+  /* draw centers */
+  GLint old_depth_mode;
+  GLboolean lighting_enabled = glIsEnabled(GL_LIGHTING);
+  if (lighting_enabled)
+    glDisable(GL_LIGHTING);
+
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  glGetIntegerv(GL_DEPTH_FUNC, &old_depth_mode);
+  glDepthFunc(GL_ALWAYS);
+  glPushAttrib(GL_CURRENT_BIT);
+  glColor3f(0.5f, 0.5f, 0.5f);
+  for (size_t i = 0; i < arrsize(centers); i++) {
+    glm::vec3& center = centers[i];
+    glTranslatef(center.x, center.y, center.z);
+    gluSphere(g_pquadratic, 0.5, 10, 10);
+    glTranslatef(-center.x, -center.y, -center.z);   
+  }
+  glPopAttrib();
+  glDepthFunc(old_depth_mode);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+  if (lighting_enabled) {
+    glEnable(GL_LIGHTING);
+  }
+}
+
+void darw_axises()
+{
+  static const glm::vec3 lines[] = {
+    { 0.f, 0.f, 0.f }, { 1.f, 0.f, 0.f },
+    { 0.f, 0.f, 0.f }, { 0.f, 1.f, 0.f },
+    { 0.f, 0.f, 0.f }, { 0.f, 0.f, 1.f }
+  };
+  glPushMatrix();
+  glLoadIdentity();
+  glPushAttrib(GL_CURRENT_BIT);
+  glColor3f(1.f, 0.f, 0.f);
+  glVertexPointer(3, GL_FLOAT, 0, glm::value_ptr(lines[0]));
+  glDrawArrays(GL_LINES, 0, 2);
+  glColor3f(0.f, 1.f, 0.f);
+  glVertexPointer(3, GL_FLOAT, 0, glm::value_ptr(lines[1]));
+  glDrawArrays(GL_LINES, 0, 2);
+  glColor3f(0.f, 0.f, 1.f);
+  glVertexPointer(3, GL_FLOAT, 0, glm::value_ptr(lines[2]));
+  glDrawArrays(GL_LINES, 0, 2);
+  glPopAttrib();
+  glPopMatrix();
+}
+
+bool stencil_buffer_available()
+{
+  GLenum status;
+  GLubyte stencil_value;
+  glReadPixels(0, 0, 1, 1, GL_STENCIL_INDEX, GL_UNSIGNED_BYTE, &stencil_value);
+  status = glGetError();
+  if (status != GL_NO_ERROR) {
+    if (status == GL_INVALID_OPERATION) {
+      /* stencil buffer is invalid */
+      printf("stencil buffer is not available\n");
+      return false;
+    }
+    printf("stencil_buffer_available(): glReadPixels() returned error: %d\n", status);
+    return false;
+  }
+  return true;
 }
 
 int main(int argc, char *argv[])
@@ -289,6 +468,11 @@ int main(int argc, char *argv[])
   }
   printf("model loaded!\n");
 
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+  SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+  SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 8);
+
   pwindow = SDL_CreateWindow("pendulum graphics", 
     SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 1024, 
     SDL_WINDOW_OPENGL|SDL_WINDOW_SHOWN|SDL_WINDOW_RESIZABLE);
@@ -296,11 +480,6 @@ int main(int argc, char *argv[])
     printf("SDL_CreateWindow() failed! Error: \"%s\"", SDL_GetError());
     return 1;
   }
-
-  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-  SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
-  SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 8);
-  SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
   ctx = SDL_GL_CreateContext(pwindow);
   if (!ctx) {
@@ -316,9 +495,21 @@ int main(int argc, char *argv[])
 
   int num_keys;
   const Uint8* pbuttons = SDL_GetKeyboardState(&num_keys);
-  glEnable(GL_DEPTH_TEST);
-  glEnable(GL_LIGHTING);
   glEnable(GL_MULTISAMPLE);
+  glEnable(GL_DEPTH_TEST);
+
+  /* enable stencil buffer */
+  printf("enabling stencil buffer...\n");
+  glEnable(GL_STENCIL_TEST);
+  glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+  glClearStencil(0);
+  glClear(GL_STENCIL_BUFFER_BIT);
+  if (!stencil_buffer_available()) {
+    printf("stencil buffer not created correctly!\n");
+    return 1;
+  }
+
+  glEnable(GL_LIGHTING);
   //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
   GLfloat mat_diffuse[] = { 0.8f, 0.8f, 0.8f, 1.0f };
@@ -339,53 +530,55 @@ int main(int argc, char *argv[])
   prepare_nodes(nodes_hierarchy, arrsize(nodes_hierarchy));
   print_childs(nodes_hierarchy, arrsize(nodes_hierarchy));
   change_coord_system(nodes_hierarchy, arrsize(nodes_hierarchy), &obj);
+  print_nodes_barycenter(&obj);
 
   model_node* ppendulum_node = find_node("node3");
+  int         pendulum_node_idx = get_node_index("node3");
+  assert(pendulum_node_idx != -1 && "node not found!");
   model_node* pload0 = find_node("node4");
   model_node* pload1 = find_node("node5");
   assert(ppendulum_node && "ppendulum_node was nullptr!");
 
-  psphere = gluNewQuadric();
+  g_pquadratic = gluNewQuadric();
 
+  glm::vec3 worldpos;
   glEnableClientState(GL_VERTEX_ARRAY);
-  glEnableClientState(GL_NORMAL_ARRAY);
+  
   while (handle_events()) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
     glLoadIdentity();
     current_time = get_time();
     glTranslatef(0.f, -10.f, -60.f);
-    glRotatef(current_time, 0.f, 1.f, 0.f);
+    glRotatef(180.f, 0.f, 1.f, 0.f);
 
-    constexpr float deg = 90.f;
+    constexpr float deg = 5.f;
     float sinv = sinf(current_time);
     ppendulum_node->set_rotation({ 0.f, 0.f, sinv * deg});
     pload0->set_position({ 0.f, sinv * 10.f, 0.f });
     pload1->set_position({ 0.f, sinv * -10.f, 0.f });
 
+    glEnableClientState(GL_NORMAL_ARRAY);
     draw_model();
+    glDisableClientState(GL_NORMAL_ARRAY);
+    //draw_centers();
+    //draw_debug_sphere(centers[pendulum_node_idx], glm::vec3(0.f, 0.f, 1.f));
 
-    /* draw centers */
-    GLint old_depth_mode;
-    glDisable(GL_LIGHTING);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glGetIntegerv(GL_DEPTH_FUNC, &old_depth_mode);
-    glDepthFunc(GL_ALWAYS);
-    for (size_t i = 0; i < arrsize(centers); i++) {
-      glm::vec3& center = centers[i];
-      glPushAttrib(GL_CURRENT_BIT);
-      glColor3f(0.f, 1.f, 0.f);
-      glTranslatef(center.x, center.y, center.z);
-      gluCylinder(psphere, 1.0, 0., 1.0, 10, 10);
-      glTranslatef(-center.x, -center.y, -center.z);
-      glPopAttrib();
+    //NOTE: K.D.  OBJECT PICKING HERE!  objectid==nodeid
+    int objectid = get_id_from_coord(worldpos, g_viewport, g_mouse);
+    if (objectid != -1) {
+      bool is_load = objectid >= 4 && objectid <= 5;
+      if (is_load) {
+        draw_debug_sphere(worldpos, glm::vec3(1.f, 1.f, 0.f));
+        printf("it is object: %d\n", objectid);
+      }
+      else if (objectid == pendulum_node_idx) {
+        draw_debug_sphere(worldpos, glm::vec3(0.f, 0.f, 0.5f));
+      }
     }
-    glDepthFunc(old_depth_mode);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    glEnable(GL_LIGHTING);
     SDL_GL_SwapWindow(pwindow);
   }
   glDisableClientState(GL_VERTEX_ARRAY);
-  glDisableClientState(GL_NORMAL_ARRAY);
+  
   SDL_GL_DeleteContext(ctx);
   SDL_DestroyWindow(pwindow);
   SDL_Quit();
